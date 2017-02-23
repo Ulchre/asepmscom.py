658f48a08e8da856038d947492cec89c2a880cd2
Ulchre <admin@asepms.com>
2017-02-23 21:19:26 +0700

    Add files via upload

---
+++ asepmscom.py
@@ -0,0 +1,9 @@
+# This is a Small Project Python by Asep Ulchre.
+# Examples for Trouble in Project Calendar.
+
+if __name__ == "__main__":
+    print "Enter The Year"
+    
+    import calendar
+    year = int(input("This Year: "))
+    calendar.prcal(year)
